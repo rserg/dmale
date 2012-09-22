dfbbae6ece7046b73ffdaaaaa7eadfd576e689aa
rserg <matarcade@yandex.ru>
2012-09-22 22:47:14 +0600

    Add class for load data from file

--- dmale.d
+++ dmale.d
@@ -1,5 +1,12 @@
-import std.stdio,std.string,std.exception,std.math, std.algorithm;
+import std.stdio,std.string,std.exception,std.math, std.algorithm,
+std.file,std.array,std.conv;
 
+
+interface ILearning
+{
+	string get();
+	int length();
+}
 template doubleTemplate(T,S){
 	alias T[][][S] KeyValues;
 	alias T[][][S] KeyValue;
@@ -11,13 +18,15 @@ template singleTemplate(T){
 	alias T[][]VArrays;
 }
 
+
+
 //TODO Parse file with matrix of params
 class ParseText
 {
 
 }
 
-//Some struct of values
+//TODO
 struct knn(T,R)
 {
 	string name;
@@ -27,8 +36,30 @@ struct knn(T,R)
 }
 
 
+//Загрузка данных в виде матрицы
+auto LoadMatrixData(string filename)
+{
+	//Not found?
+	auto file = File(filename,"r");
+	float dataArray[][];
+	foreach(line;file.byLine())
+	{
+		float lineData[];
+		foreach(num;line.split(" "))
+		{
+			lineData~=parse!float(num);
+		}
+
+		dataArray~= lineData;
+	}
+
+	assert(dataArray.length > 0);
+
+    return dataArray;
+}
+
 class Attributes{}
-class LoadKNN(T,V=singleTemplate!(T).VArray)
+class LoadKNN(T,V=singleTemplate!(T).VArray):ILearning
 {
 	this(V[] arr, V cand)
 	{
@@ -113,6 +144,63 @@ class LoadKNN(T,V=singleTemplate!(T).VArray)
 	V[]set;
 }
 
+class Perceptron(T):ILearning
+{
+  T[]data;
+  this(T[] data)
+  {
+  	this.data = data;
+  }
+
+  T get(string[]names)
+  {
+  	for(int i = 0;i < data.length;++i)
+  	{
+
+  	}
+  }
+ int length()
+ {
+
+ }
+}
+
+/*Bayes area*/
+//http://oscarbonilla.com/2009/05/visualizing-bayes-theorem/
+//P(A|B) = P(B|A)*P(A)/P(B)
+//http://en.wikipedia.org/wiki/Naive_Bayes_classifier
+class NativeBayesClassifier(T):ILearning
+{
+	T[]data;
+	this(T[]data)
+	{
+		this.data = data;
+	}
+
+	int length()
+	{
+		return data.length;
+	}
+
+	float calculate(T num)
+	{
+		return 0.0;
+	}
+
+
+}
+
+private float bayis()
+{
+	float a0 = 0.01;
+	float a1 = 0.8;
+	float a2 = 0.2;
+	float a3 = 0.96;
+
+	float pa = ((1 - 0.8) * a3) + (a1 * a0);
+	return a0 * a1/pa;
+}
+
 unittest {
 	auto tbit = [0.4,0.8,0.4];
 	auto bits = [[0.1,0.5,0.6],[0.8,0.4,0.7],[0.7,0.8,0.2], [0.2,0.3,0.7]];
@@ -122,8 +210,12 @@ unittest {
     writeln(ld.get(["olg", "olg", "told", "told"]));
 }
 
+unittest {
+	auto ld = new Perceptron!double();
+}
+
 void main()
 {
-
-	
+ 
+ 
 }
